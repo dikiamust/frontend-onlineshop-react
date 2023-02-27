690c3b1fe7cf964d57b4953b12a28cc99dbca1b7
dikiamust <agungmustaqimdiki@gmail.com>
2023-02-27 08:10:17 +0700

    remove unnecessary code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -55,18 +55,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <ThemeProvider>
         <Component {...pageProps} />
       </ThemeProvider>
-
-      {/* <!-- Hotjar Tracking Code for http://natureworkslab.com/ --> */}
-      <Script id="hotjar" strategy="afterInteractive">
-        {`(function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3121864,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-      </Script>
     </>
   );
 }
